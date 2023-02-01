c6599c61aa44c2414f9d92f1ffcf53428b342e0c
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-02-01 20:23:36 -0300

    findById endpoint for books

--- src/main/java/com/juniorsilvacc/erudio/controllers/BookController.java
+++ src/main/java/com/juniorsilvacc/erudio/controllers/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class BookController {
 	@GetMapping(value = "/")
 	public List<BookDTO> findAll() {
 		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public BookDTO findById(@PathVariable Long id) {
+		return service.findById(id);
 	}
 
 }
--- src/main/java/com/juniorsilvacc/erudio/services/BookService.java
+++ src/main/java/com/juniorsilvacc/erudio/services/BookService.java
@@ -1,12 +1,17 @@
 package com.juniorsilvacc.erudio.services;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.juniorsilvacc.erudio.controllers.PersonController;
 import com.juniorsilvacc.erudio.dtos.BookDTO;
+import com.juniorsilvacc.erudio.exceptions.ResourceNotFoundException;
 import com.juniorsilvacc.erudio.models.Book;
 import com.juniorsilvacc.erudio.repositories.BookRepository;
 
@@ -21,7 +26,22 @@ public class BookService {
 		
 		var listBooks = books.stream().map(BookDTO::new).collect(Collectors.toList());
 		
+		listBooks.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getId())).withSelfRel()));
+		
 		return listBooks;
 	}
+
+	public BookDTO findById(Long id) {
+		Book entity = repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		BookDTO dto = new BookDTO(entity);
+		
+		dto.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		
+		return dto;
+	}
+	
+	
 
 }
