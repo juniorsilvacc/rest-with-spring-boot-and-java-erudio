0f19b46fc9e7e72bfe64e96790067b360025c4af
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-02-09 02:16:25 -0300

    implementing the service responsible for authentication

--- src/main/java/com/juniorsilvacc/erudio/services/AuthService.java
+++ src/main/java/com/juniorsilvacc/erudio/services/AuthService.java
@@ -1,5 +1,53 @@
 package com.juniorsilvacc.erudio.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.juniorsilvacc.erudio.dtos.security.AccountCredentialsDTO;
+import com.juniorsilvacc.erudio.dtos.security.TokenDTO;
+import com.juniorsilvacc.erudio.repositories.UserRepository;
+import com.juniorsilvacc.erudio.securityJwt.JwtTokenProvider;
+
+@Service
 public class AuthService {
 
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public ResponseEntity<?> signin(AccountCredentialsDTO data) {
+		try {
+			var username = data.getUserName();
+			var password = data.getPassword();
+			
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(username, password));
+			
+			var user = repository.findByUserName(username);
+			
+			var tokenResponse = new TokenDTO();
+			
+			if(user != null) {
+				tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+			} else {
+				throw new UsernameNotFoundException("Username " + username + " not found");
+			}
+			
+			return ResponseEntity.ok(tokenResponse);
+			
+		} catch (Exception e) {
+			throw new BadCredentialsException("Invalid username/password suppliend");
+		}
+	}
+	
 }
